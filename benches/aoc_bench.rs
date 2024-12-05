3c21158951802048e4cde5244d14d0d775498857
plesak <mikulas.plesak@gmail.com>
2024-12-05 10:42:27 -0500

    fix gha

--- benches/aoc_bench.rs
+++ benches/aoc_bench.rs
@@ -1,38 +1,38 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 
 fn bench_day1_part1(c: &mut Criterion) {
-    let input = include_str!("../input_files/y24q1q1.txt");
+    let input = include_str!("../inputs/day1.txt");
     c.bench_function("day1_part1", |b| b.iter(|| advent_of_code::day1::part1(input)));
 }
 fn bench_day1_part2(c: &mut Criterion) {
-    let input = include_str!("../input_files/y24q1q2.txt");
+    let input = include_str!("../inputs/day1.txt");
     c.bench_function("day1_part2", |b| b.iter(|| advent_of_code::day1::part2(input)));
 }
 
 fn bench_day2_part1(c: &mut Criterion) {
-    let input = include_str!("../input_files/y24q2q1.txt");
+    let input = include_str!("../inputs/day2.txt");
     c.bench_function("day2_part1", |b| b.iter(|| advent_of_code::day2::part1(input)));
 }
 fn bench_day2_part2(c: &mut Criterion) {
-    let input = include_str!("../input_files/y24q2q2.txt");
+    let input = include_str!("../inputs/day2.txt");
     c.bench_function("day2_part2", |b| b.iter(|| advent_of_code::day2::part2(input)));
 }
 
 fn bench_day3_part1(c: &mut Criterion) {
-    let input = include_str!("../input_files/y24q3q1.txt");
+    let input = include_str!("../inputs/day3.txt");
     c.bench_function("day3_part1", |b| b.iter(|| advent_of_code::day3::part1(input)));
 }
 fn bench_day3_part2(c: &mut Criterion) {
-    let input = include_str!("../input_files/y24q3q2.txt");
+    let input = include_str!("../inputs/day3.txt");
     c.bench_function("day3_part2", |b| b.iter(|| advent_of_code::day3::part2(input)));
 }
 
 fn bench_day4_part1(c: &mut Criterion) {
-    let input = include_str!("../input_files/y24q4q1.txt");
+    let input = include_str!("../inputs/y24q4q1.txt");
     c.bench_function("day4_part1", |b| b.iter(|| advent_of_code::day4::part1(input)));
 }
 fn bench_day4_part2(c: &mut Criterion) {
-    let input = include_str!("../input_files/y24q4q2.txt");
+    let input = include_str!("../inputs/y24q4q2.txt");
     c.bench_function("day4_part2", |b| b.iter(|| advent_of_code::day4::part2(input)));
 }
 
