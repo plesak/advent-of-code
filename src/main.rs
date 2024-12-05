f95d4f1d52e5b8d4b48aac18568070bbec150383
plesak <mikulas.plesak@gmail.com>
2024-12-05 17:35:08 -0500

    reformat

--- src/main.rs
+++ src/main.rs
@@ -8,116 +8,143 @@ mod day2;
 mod day3;
 mod day4;
 
+use clap::Parser;
+use std::path::PathBuf;
 use std::time::Instant;
-use std::{env, fs, io};
+use std::fs;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Day number (e.g., day1, day2, ...)
+    #[arg(required = true)]
+    day: String,
+
+    /// Part number (part1 or part2)
+    #[arg(required = true)]
+    part: String,
+
+    /// Year (optional e.g. 2023, defaults to current year)
+    #[arg(short, long)]
+    year: Option<String>,
+
+    /// Use example input
+    #[arg(short, long)]
+    example: bool,
+}
 
 fn main() {
-    let mut args = env::args();
+    let args = Args::parse();
 
-    if args.len() < 2 {
-        panic!("Usage: cargo run <question_id> [-r]");
+    let year = args.year.unwrap_or_else(|| "latest".to_string());
+    let input_file = construct_input_path(&args.day, &args.part, &year, args.example);
+
+    println!("Input file path: {:?}", input_file);
+    let q = format!("{}_{}", args.day, args.part);
+
+    let mut input = fs::read_to_string(input_file).unwrap();
+
+    if args.example {
+        let lines: Vec<&str> = input.lines().collect();
+        if let Some((last, rest)) = lines.split_last() {
+            let expected_result = last.trim().to_string();
+            input = rest.join("\n");
+            println!("Expected result: {}", expected_result);
+        } else {
+            println!("Error: Example file is empty or malformed");
+        }
     }
 
-    let _ = args.next();
-    let question_id = args.next().unwrap();
-    let full = args.next().map_or(false, |arg| arg == "-f");
-    let suffix = if full { ".txt" } else { "_test.txt" };
-
-    let inp: String;
-    if !full {
-        let input: String = read_file(&question_id, suffix).unwrap();
-        let (inpt, true_result) = input.split_at(input.rfind('\n').unwrap());
-        inp = inpt.to_string();
-        println!("Expected result: {}", true_result.chars().skip(1).collect::<String>());
-    } else {
-        inp = read_file(&question_id, suffix).unwrap();
-    }
-
-    let yr = question_id.chars().take(3).collect::<String>();
     let result:String;
     let start_time = Instant::now();
-    if yr == "y24" {
-        result = match question_id.as_str() {
-            "y24q1q1" => day1::part1(&inp).to_string(),
-            "y24q1q2" => day1::part2(&inp).to_string(),
-            "y24q2q1" => day2::part1(&inp).to_string(),
-            "y24q2q2" => day2::part2(&inp).to_string(),
-            "y24q3q1" => day3::part1(&inp).to_string(),
-            "y24q3q2" => day3::part2(&inp).to_string(),
-            "y24q4q1" => day4::part1(&inp).to_string(),
-            "y24q4q2" => day4::part2(&inp).to_string(),
-            _ => panic!("Question not implemented! {}", question_id),
+    if year == "latest" {
+        result = match q.as_str() {
+            "day1_part1" => day1::part1(&input).to_string(),
+            "day1_part2" => day1::part2(&input).to_string(),
+            "day2_part1" => day2::part1(&input).to_string(),
+            "day2_part2" => day2::part2(&input).to_string(),
+            "day3_part1" => day3::part1(&input).to_string(),
+            "day3_part2" => day3::part2(&input).to_string(),
+            "day4_part1" => day4::part1(&input).to_string(),
+            "day4_part2" => day4::part2(&input).to_string(),
+            _ => panic!("Question not implemented! Arguments: {}", q),
         }
-    } else if yr == "y23" {
-        let input = inp.lines().map(String::from).collect();
-        result = match question_id.as_str() {
-            "y23q1q1" => y23q1q1(input),
-            "y23q1q2" => y23q1q2(input),
-            "y23q2q1" => y23q2q1(input),
-            "y23q2q2" => y23q2q2(input),
-            "y23q3q1" => y23q3q1(input),
-            "y23q3q2" => y23q3q2(input),
-            "y23q4q1" => y23q4q1(input),
-            "y23q4q2" => y23q4q2(input),
-            "y23q5q1" => y23q5q1(input),
-            "y23q5q2" => y23q5q2(input),
-            "y23q6q1" => y23q6q1(input),
-            "y23q6q2" => y23q6q2(input),
-            "y23q7q1" => y23q7q1(input),
-            "y23q7q2" => y23q7q2(input),
-            "y23q8q1" => y23q8q1(input),
-            "y23q8q2" => y23q8q2(input),
-            "y23q9q1" => y23q9q1(input),
-            "y23q9q2" => y23q9q2(input),
-            "y23q10q1" => y23q10q1(input),
-            "y23q10q2" => y23q10q2(input),
-            "y23q11q1" => y23q11q1(input),
-            "y23q11q2" => y23q11q2(input),
-            "y23q12q1" => y23q12q1(input),
-            "y23q12q2" => y23q12q2(input),
-            "y23q13q1" => y23q13q1(input),
-            "y23q13q2" => y23q13q2(input),
-            "y23q14q1" => y23q14q1(input),
-            "y23q14q2" => y23q14q2(input),
-            "y23q15q1" => y23q15q1(input),
-            "y23q15q2" => y23q15q2(input),
-            "y23q16q1" => y23q16q1(input),
-            "y23q16q2" => y23q16q2(input),
-            "y23q17q1" => y23q17q1(input),
-            "y23q17q2" => y23q17q2(input),
-            "y23q18q1" => y23q18q1(input),
-            "y23q18q2" => y23q18q2(input),
-            "y23q19q1" => y23q19q1(input),
-            "y23q19q2" => y23q19q2(input),
-            "y23q20q1" => y23q20q1(input),
-            "y23q20q2" => y23q20q2(input),
-            "y23q21q1" => y23q21q1(input),
-            "y23q21q2" => y23q21q2(input),
-            "y23q22q1" => y23q22q1(input),
-            "y23q22q2" => y23q22q2(input),
-            "y23q23q1" => y23q23q1(input),
-            "y23q23q2" => y23q23q2(input),
-            "y23q24q1" => y23q24q1(input),
-            "y23q24q2" => y23q24q2(input),
-            "y23q25q1" => y23q25q1(input),
-            "y23q25q2" => y23q25q2(input),
-            _ => panic!("Question not implemented: {}", question_id),
+    } else if year == "y23" {
+        let inp = input.lines().map(String::from).collect::<Vec<String>>();
+        result = match q.as_str() {
+            "y23q1q1" => y23q1q1(inp),
+            "y23q1q2" => y23q1q2(inp),
+            "y23q2q1" => y23q2q1(inp),
+            "y23q2q2" => y23q2q2(inp),
+            "y23q3q1" => y23q3q1(inp),
+            "y23q3q2" => y23q3q2(inp),
+            "y23q4q1" => y23q4q1(inp),
+            "y23q4q2" => y23q4q2(inp),
+            "y23q5q1" => y23q5q1(inp),
+            "y23q5q2" => y23q5q2(inp),
+            "y23q6q1" => y23q6q1(inp),
+            "y23q6q2" => y23q6q2(inp),
+            "y23q7q1" => y23q7q1(inp),
+            "y23q7q2" => y23q7q2(inp),
+            "y23q8q1" => y23q8q1(inp),
+            "y23q8q2" => y23q8q2(inp),
+            "y23q9q1" => y23q9q1(inp),
+            "y23q9q2" => y23q9q2(inp),
+            "y23q10q1" => y23q10q1(inp),
+            "y23q10q2" => y23q10q2(inp),
+            "y23q11q1" => y23q11q1(inp),
+            "y23q11q2" => y23q11q2(inp),
+            "y23q12q1" => y23q12q1(inp),
+            "y23q12q2" => y23q12q2(inp),
+            "y23q13q1" => y23q13q1(inp),
+            "y23q13q2" => y23q13q2(inp),
+            "y23q14q1" => y23q14q1(inp),
+            "y23q14q2" => y23q14q2(inp),
+            "y23q15q1" => y23q15q1(inp),
+            "y23q15q2" => y23q15q2(inp),
+            "y23q16q1" => y23q16q1(inp),
+            "y23q16q2" => y23q16q2(inp),
+            "y23q17q1" => y23q17q1(inp),
+            "y23q17q2" => y23q17q2(inp),
+            "y23q18q1" => y23q18q1(inp),
+            "y23q18q2" => y23q18q2(inp),
+            "y23q19q1" => y23q19q1(inp),
+            "y23q19q2" => y23q19q2(inp),
+            "y23q20q1" => y23q20q1(inp),
+            "y23q20q2" => y23q20q2(inp),
+            "y23q21q1" => y23q21q1(inp),
+            "y23q21q2" => y23q21q2(inp),
+            "y23q22q1" => y23q22q1(inp),
+            "y23q22q2" => y23q22q2(inp),
+            "y23q23q1" => y23q23q1(inp),
+            "y23q23q2" => y23q23q2(inp),
+            "y23q24q1" => y23q24q1(inp),
+            "y23q24q2" => y23q24q2(inp),
+            "y23q25q1" => y23q25q1(inp),
+            "y23q25q2" => y23q25q2(inp),
+            _ => panic!("Question not implemented! Arguments: {}", q),
         };
     } else {
-        panic!("Unknown year {} - only available `y23qXqX` and `y24qXqX`", yr);
+        panic!("Unknown year {} - only available `-2023` and `-2024` (default)", year);
     }
 
-    println!("Result of {}: {}", question_id, result);
+    println!("Result of: {}", result);
     println!("Time taken: {}", start_time.elapsed().as_secs_f64());
 }
 
-fn read_file(q_id: &str, suffix: &str) -> io::Result<String> {
-    let current_exe = env::current_exe().expect("Failed to get current exe path");
-    let project_root = current_exe.parent().unwrap().parent().unwrap().parent().unwrap();
-    let filename = project_root
-        .join("input_files").join(format!("{}{}", q_id, suffix));
-    println!("Reading {}", filename.display());
-    fs::read_to_string(filename)
-}
+fn construct_input_path(day: &str, part: &str, year: &str, example: bool) -> PathBuf {
+    let mut path = PathBuf::from("inputs");
 
-// NICE
+    if year != "latest" {
+        path.push(year);
+    }
+
+    if example {
+        path.push("tests");
+        path.push(format!("{}_{}.txt", day, part));
+    } else {
+        path.push(format!("{}.txt", day));
+    }
+
+    path
+}
